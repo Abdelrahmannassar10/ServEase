6c930816800f5f402dd95c7edf617211c7a4a776
Abdelrahman Nassar <atef18008@gmail.com>
2026-05-04 22:17:50 +0300

    test

--- src/common/helper/cron-job.helper.ts
+++ src/common/helper/cron-job.helper.ts
@@ -10,15 +10,20 @@ export class TasksService {
     private readonly userRepository: UserRepository,
   ) {}
 
-  @Cron('* * 6 * * *')
-  async handleCron() {
-    this.logger.debug('Called when the current second is 45');
-    log('Called when the current second is 45');
-    await this.userRepository.updateMany(
-      { otpExpiry: { $lte: new Date() } },
-      { $unset:{otp:"",otpExpiry:""}},
-    );
-  }
+
+
+
+
+
+  // @Cron('* * 6 * * *')
+  // async handleCron() {
+  //   this.logger.debug('Called when the current second is 45');
+  //   log('Called when the current second is 45');
+  //   await this.userRepository.updateMany(
+  //     { otpExpiry: { $lte: new Date() } },
+  //     { $unset:{otp:"",otpExpiry:""}},
+  //   );
+  // }
 
   @Cron('* * 6 * * *')
   async softDeleteCron() {
