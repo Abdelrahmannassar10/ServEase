891ae35093bcade7daef661c49535b4274cfce74
Abdalrahaman  Nassar <113613424+Abdelrahmannassar10@users.noreply.github.com>
2026-06-28 01:57:38 +0300

    chatbot action fix

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -3,6 +3,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { AuthModule } from '@modules/auth/auth.module';
 import { UserMongooseModule } from '@shared/modules';
+import { ServiceRequestModule } from '@modules/service-request/service-request.module';
+import { ServiceModule } from '@modules/service/service.module';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { ChatSessionMongooseModule } from '@shared/modules';
@@ -12,6 +14,8 @@ import { ChatSessionMongooseModule } from '@shared/modules';
     ChatSessionMongooseModule,
     UserMongooseModule,
     AuthModule,
+    ServiceRequestModule,
+    ServiceModule,
     JwtModule.registerAsync({
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({ secret: config.get('JWT_SECRET') }),
